3f5c74c6055eec92c061070b8e2b782315dc1147
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-12-19 21:30:40 +0100

    bug: error test 2.4

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,19 +1,14 @@
 import { app as server } from "./app";
 import { env } from "@/core/config/env.config";
-import { runningController } from "@/core/hooks/running-controller";
 import { Chalk } from "@/core/config/chalk.config";
 
 const { PORT } = env;
 
 server.listen(parseInt(PORT), () => {
-	runningController().then(async isCanRunning => {
+	(async () => {
         const chalk = await Chalk();
         console.log(" ");
-        if (!isCanRunning) {
-            console.log(chalk.bold.redBright("❌ Server cannot start, shutting down."));
-            return process.exit(1); 
-        }
         console.log(chalk.bold.greenBright(`💥 Server running at ${chalk.underline(`http//127.0.0.1:${PORT}/`)}`));
         console.log(" ");
-    });
+    })();
 });
